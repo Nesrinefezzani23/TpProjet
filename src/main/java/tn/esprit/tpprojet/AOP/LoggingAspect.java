587ae3d267c1c9460fbdf1b336bd58693164b8f2
nesrine <nesrine.fezzani@esprit.tn>
2025-12-02 10:28:47 +0100

    AOP ProjetService

---
+++ src/main/java/tn/esprit/tpprojet/AOP/LoggingAspect.java
@@ -0,0 +1,18 @@
+package tn.esprit.tpprojet.AOP;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Slf4j
+public class LoggingAspect {
+    @Before("execution(* tn.esprit.tpprojet.services.ProjetServiceImpl.findAllProjet(..))")
+    public void logMethodEntry(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        log.info("In method " + name + " : ");
+    }
+}
